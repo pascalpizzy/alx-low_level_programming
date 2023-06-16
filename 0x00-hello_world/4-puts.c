230784de8a62a1bd83c520e555fb75b2368ac442
pascalpizzy <pascalikechukwu@gmail.com>
2023-06-15 17:12:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  * main - the main serves as the entry point
  *
  * return: this will return 0 if the execution is successful
+ * description: this script prints a statement using puts
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+		return (0);
 }
