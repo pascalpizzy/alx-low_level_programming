25d93ce47085edd3b9e8d8b55c4608f636ca4b80
pascalpizzy <pascalikechukwu@gmail.com>
2023-06-16 18:16:01 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,12 @@
  */
 int main(void)
 {
-	char ch;
+	int ch;
 
-	for(ch = 'a'; ch <= 'z; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	for(ch = 'A'; ch <= 'Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
-	
 	putchar('\n');
 	return (0);
 }
