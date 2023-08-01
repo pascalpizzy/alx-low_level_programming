84c69d40af77d98fb8c3ed0101b5d38dfc62f964
pascalpizzy <pascalikechukwu@gmail.com>
2023-07-31 17:40:05 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,9 @@
 #include "lists.h"
 
 /** 
- * Function print_listint: to print all elements of a listint_t list
+ * Function print_listint - to print all elements of a listint_t list
+ * @h: head list
+ *
  * Return: The total numbers of the nodes 
  */
 size_t print_listint(const listint_t *h) {
