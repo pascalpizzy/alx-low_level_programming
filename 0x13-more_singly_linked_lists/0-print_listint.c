f5ae2b3059bff12b15cc18ba6cddefd2647c20a9
pascalpizzy <pascalikechukwu@gmail.com>
2023-07-31 17:48:33 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,18 +6,16 @@
  *
  * Return: The total numbers of the nodes 
  */
-size_t print_listint(const listint_t *h) 
+size_t print_listint(const listint_t *h)
 {
+	size_t count = 0;
 
-    size_t count = 0;
-
-    while (h != NULL) 
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-
-    return count;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
 
